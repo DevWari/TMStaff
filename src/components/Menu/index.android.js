b95a0fc348fca964ab769840ace1c97e317b5b1a
LyDoan <lyvanna1987@outlook.com>
2020-11-17 04:44:28 +0800

    update Dashboard

---
+++ src/components/Menu/index.android.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import styled from 'styled-components/native';
+import { navigate } from 'src/utils/navigation';
+import Icon from 'react-native-vector-icons/Ionicons'
+import {drawer} from 'src/App'
+
+const Header = (props) => {  
+
+  const onPressNotification = () => {
+    navigate('MyNotificationScreen');
+  }
+  return (
+    <Container> 
+      
+      <View /> 
+      { props.title && <Title>{props.title}</Title> }
+      { props.message ? 
+        <TouchableOpacity onPress={()=>onPressNotification()}>
+          <Image 
+            source={require('src/assets/img/dashboard/bell-alert.png')} 
+            style={{width: 30, height: 30}} />
+          </TouchableOpacity>:
+        <View />       
+      }
+    </Container>
+  );
+};
+
+export default Header;
+
+const Container = styled(View)`
+  width: 100%;
+  height: 50px;
+  background-color: #15892E;
+  justify-content: space-between;
+  flex-direction: row;
+  padding-horizontal: 20px;
+  align-items: center;
+`;
+const Title = styled(Text)`
+  color: black;
+  font-size: 25px;
+  font-weight: bold;
+  text-align: center;
+`;
---
+++ src/screens/Dashboard/Content/index.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Linking
+} from 'react-native'
+import styled from 'styled-components/native'
+import { Colors } from 'src/theme';
+import { navigate } from 'src/utils/navigation';
+
+const Content = (props) => {
+  return (
+    <Container>
+      <TabBarContainer>
+        <TabButton style={{backgroundColor: 'red'}}>
+          <TabBarTitle>Clock in/out</TabBarTitle>
+        </TabButton>
+        <TabButton style={{backgroundColor: '#15892E'}}>
+          <TabBarTitle>Today's Jobs</TabBarTitle>
+        </TabButton>
+      </TabBarContainer>
+      <Row>
+        <StyleButton onPress={()=>navigate('MyEstimate')}>
+            <Image source={require('src/assets/img/dashboard/dash2.png')} />
+            <Title>My Schedule</Title>
+        </StyleButton>
+        <StyleButton onPress={()=> navigate("MyAppointment", {hashed_id: null})}>
+            <Image source={require('src/assets/img/dashboard/dash4.png')} />
+            <Title>Courses</Title>
+        </StyleButton>
+      </Row>
+      <Row>
+        <StyleButton onPress={() => navigate('MyMessageScreen')} bgColor={Colors.textColor}>
+            <Image source={require('src/assets/img/dashboard/dash3.png')} />
+            <Title>Messages</Title>
+        </StyleButton>
+        <StyleButton onPress={() => navigate('MyEstimate')}>
+            <Image source={require('src/assets/img/dashboard/dash5.png')} />
+             <Title>Forums</Title>
+        </StyleButton>
+      </Row>
+      
+
+    </Container>
+  )
+}
+
+export default Content
+
+const Container = styled(View)`
+  width: 100%;
+  height: 480px;
+  align-items: center;  
+`
+const StyleButton = styled(TouchableOpacity)`
+  shadow-color: #000;
+  shadow-opacity: 0.8;
+  shadow-radius: 3.87px;
+  shadow-offset: 0 2px;
+  elevation: 5;
+  width: 47%;
+  height: 170px;
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 10px;
+  align-items: center;
+  margin-bottom: 30px;
+  background-color : #fff 
+`
+//background color must be set
+const Row = styled(View)`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-horizontal: 20px; 
+  margin-top: 10px;
+`
+const Title = styled(Text)`
+  font-size: 15px;
+  margin-top: 15px;
+`
+const TabBarContainer = styled (View)`
+  width: 100%;
+  height: 60px;
+  flex-direction: row;
+  justify-content: space-between;
+`
+const TabButton = styled(TouchableOpacity)`
+  width: 48.5%;  
+  justify-content: center;
+  align-items: center;
+`
+const TabBarTitle = styled(Text)`
+  color:  white;
+  font-size: 18px;
+`
