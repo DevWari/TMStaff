03af708182b8cb8d5e02990dcfae03fff1c69489
LyDoan <lyvanna1987@outlook.com>
2020-11-17 02:15:07 +0800

    Structuring

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+import {View, Text} from 'react-native'
+const App = () => {
+  return (
+    <View>
+      <Text>aaaaaaa</Text>
+    </View>    
+  )
+}
+
+export default App
